0494a70fc848367db1e07664992028a05ad8476b
Sugut <sugutvictorine@gmail.com>
2022-10-25 15:13:11 +0300

    done assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge= []
+    end
+    def learn lesson
+        @knowledge << lesson
+    end
+    def knowledge
+        @knowledge
+    end
 end
